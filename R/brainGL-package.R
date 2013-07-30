ec2bfd4e18c018eb04de991550ecb7fa1e13c3ee
muschellij2 <muschellij2@gmail.com>
2013-07-30 01:09:39 -0400

    Added Documentation for Data set

--- R/brainGL-package.R
+++ R/brainGL-package.R
@@ -1,4 +1,24 @@
-#' blah blah
+#' Information
+#' Brain Template from Copyright (C) 1993–2009 Louis Collins, McConnell Brain Imaging Centre, 
+#' Montreal Neurological Institute, McGill University
+#' 6th generation non-linear symmetric brain
+#' 
+#' @name MNI_Template
+#' @docType data
+#' @references \url{http://www.bic.mni.mcgill.ca/ServicesAtlases/ICBM152NLin6}
+#' @keywords data
+NULL
+
+#' @name brainGL-package
+#' @aliases brainGL
+#' @docType package
+#' @title Functions to render 3D brain images in html
+#' @author \email{muschellij2@@gmail.com}
+#' @keywords package, brainGL
+#' @seealso \code{\link{misc3d}}, \code{\link{rgl}}
+NULL
+
+#' 
 #' @include 'makeScene.R'
 #' @include 'write4D.file.R'
 #' @include 'write4D.R'
--- R/writeWebGL_split.R
+++ R/writeWebGL_split.R
@@ -9,6 +9,9 @@
 #' @param ... - further arguments passed to \link[rgl]{writeWebGL}
 #' @export
 #' @examples
+#' Brain Template from Copyright (C) 1993–2009 Louis Collins, McConnell Brain Imaging Centre, 
+#' Montreal Neurological Institute, McGill University
+#' 6th generation non-linear symmetric brain
 #' template <- readNIfTI(system.file("MNI152_T1_2mm_brain.nii.gz", package="brainGL")
 #' , reorient=FALSE) 
 #' dtemp <- dim(template)
